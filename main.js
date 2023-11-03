44e7c73c44c07346f569155a6833c6e78cadd418
Jone <jone.chong99@gmail.com>
2023-11-03 10:11:04 +0800

    feat: set gpt logo as app icon

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
 function createWindow() {
     // Create the browser window.
     let win = new BrowserWindow({
         width: 800,
         height: 600,
+        icon: path.join(__dirname, "assets/ChatGPT_logo.png"),
         webPreferences: {
             nodeIntegration: false,
         },
